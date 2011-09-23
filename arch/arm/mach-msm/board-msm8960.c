00e2a1f49886ac828ee030ef37a850af1e2351b8
Laura Abbott <lauraa@codeaurora.org>
2011-09-22 20:59:12 -0700

    gpu: ion: Add support for iommus
    Add infrastructure to support mapping allocations
    into iommus.
    
    Change-Id: Ia5eafebee408e297013bf55284abf67d9eb8d78b
    Signed-off-by: Laura Abbott <lauraa@codeaurora.org>
    
    Conflicts:
    
    	arch/arm/mach-msm/board-msm8960.c
    	include/linux/ion.h

--- arch/arm/mach-msm/board-msm8960.c
+++ arch/arm/mach-msm/board-msm8960.c
@@ -9,11 +9,14 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
+<<<<<<< HEAD
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
  * 02110-1301, USA.
  *
+=======
+>>>>>>> 8c01736... gpu: ion: Add support for iommus
  */
 #include <linux/kernel.h>
 #include <linux/platform_device.h>
@@ -88,4 +91,3 @@ MACHINE_START(MSM8960_RUMI3, "QCT MSM8960 RUMI3")
 	.timer = &msm_timer,
 	.init_machine = msm8960_rumi3_init,
 MACHINE_END
-
