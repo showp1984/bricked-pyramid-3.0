9f1a05789e280c6aca1c4bf192a412df8279055c
James Sullins <jcsullins@gmail.com>
2012-01-23 03:15:52 -0600

    rework gcc version check for '.arch_extension sec'

--- arch/arm/mach-msm/scm.c
+++ arch/arm/mach-msm/scm.c
@@ -36,9 +36,15 @@ module_param_named(simlock_code, simlock_code, charp, S_IRUGO | S_IWUSR | S_IWGR
 #define SCM_ERROR		-1
 #define SCM_INTERRUPTED		1
 
-#define GCC_VERSION (__GNUC__ * 10000 \
-                               + __GNUC_MINOR__ * 100 \
-                               + __GNUC_PATCHLEVEL__)
+#if defined(__GNUC__) && \
+	defined(__GNUC_MINOR__) && \
+	defined(__GNUC_PATCHLEVEL__) && \
+	((__GNUC__ * 10000 + __GNUC_MINOR__ * 100 + __GNUC_PATCHLEVEL__)) \
+		> 40400
+#define USE_ARCH_EXTENSION_SEC 1
+#else
+#define USE_ARCH_EXTENSION_SEC 0
+#endif
 
 static DEFINE_MUTEX(scm_lock);
 
@@ -204,7 +210,7 @@ static u32 smc(u32 cmd_addr)
 			__asmeq("%1", "r0")
 			__asmeq("%2", "r1")
 			__asmeq("%3", "r2")
-#if GCC_VERSION > 40400
+#if USE_ARCH_EXTENSION_SEC
 			".arch_extension sec\n"
 #endif
 			"smc	#0	@ switch to secure world\n"
@@ -328,7 +334,7 @@ s32 scm_call_atomic1(u32 svc, u32 cmd, u32 arg1)
 		__asmeq("%1", "r0")
 		__asmeq("%2", "r1")
 		__asmeq("%3", "r2")
-#if GCC_VERSION > 40400
+#if USE_ARCH_EXTENSION_SEC
 		".arch_extension sec\n"
 #endif
 		"smc	#0	@ switch to secure world\n"
@@ -363,7 +369,7 @@ s32 scm_call_atomic2(u32 svc, u32 cmd, u32 arg1, u32 arg2)
 		__asmeq("%2", "r1")
 		__asmeq("%3", "r2")
 		__asmeq("%4", "r3")
-#if GCC_VERSION > 40400
+#if USE_ARCH_EXTENSION_SEC
 		".arch_extension sec\n"
 #endif
 		"smc	#0	@ switch to secure world\n"
@@ -393,7 +399,7 @@ u32 scm_get_version(void)
 			__asmeq("%1", "r1")
 			__asmeq("%2", "r0")
 			__asmeq("%3", "r1")
-#if GCC_VERSION > 40400
+#if USE_ARCH_EXTENSION_SEC
 			".arch_extension sec\n"
 #endif
 			"smc	#0	@ switch to secure world\n"
