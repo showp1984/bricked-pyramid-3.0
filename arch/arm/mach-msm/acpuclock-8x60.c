2d519e08c188f0bda1d8da9bc386ec9c82150c86
showp1984 <showp1984@gmail.com>
2012-03-19 03:34:59 +0100

    acpu: fix typo

--- arch/arm/mach-msm/acpuclock-8x60.c
+++ arch/arm/mach-msm/acpuclock-8x60.c
@@ -801,7 +801,7 @@ static unsigned int __init select_freq_plan(void)
 	uint32_t max_khz;
 	struct clkctl_acpu_speed *f;
 
-	max_khz = 15120000;
+	max_khz = 1914000;
 	acpu_freq_tbl = acpu_freq_tbl_oc;
 
 	/* Truncate the table based to max_khz. */
