10efc7f70e2dcb5429a543f22285973f6ec91adc
showp1984 <showp1984@gmail.com>
2012-03-19 08:15:02 +0100

    kgsl: OC 3D core to 320Mhz

--- arch/arm/mach-msm/devices-msm8x60.c
+++ arch/arm/mach-msm/devices-msm8x60.c
@@ -910,7 +910,7 @@ static struct resource kgsl_3d0_resources[] = {
 static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 	.pwrlevel = {
 		{
-			.gpu_freq = 266667000,
+			.gpu_freq = 320000000,
 			.bus_freq = 4,
 			.io_fraction = 0,
 		},
