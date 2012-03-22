e32e7282055dabf71b07ffb9c938cb426a7d8122
Todd Poynor <toddpoynor@google.com>
2012-03-22 15:36:44 -0700

    power: cpufreq interactive governor: use default sample time 20ms
    Lower the default time at which a higher speed is allowed to run
    before lowering based on lower CPU load from 80ms to 20ms.  Most
    Android devices should trade power for performance here,
    although tablets and non-battery-powered devices may want to
    override this default.
    
    Change-Id: I1a4f7faeca12793c51d5b92db30a63cca8d4f1be
    Signed-off-by: Todd Poynor <toddpoynor@google.com>

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -67,7 +67,7 @@ static unsigned long go_hispeed_load;
 /*
  * The minimum amount of time to spend at a frequency before we can ramp down.
  */
-#define DEFAULT_MIN_SAMPLE_TIME 80 * USEC_PER_MSEC;
+#define DEFAULT_MIN_SAMPLE_TIME 20 * USEC_PER_MSEC;
 static unsigned long min_sample_time;
 
 /*
