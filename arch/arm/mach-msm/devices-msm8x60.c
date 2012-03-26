1288640de0719a8ef6c89798459743173fbd62ba
showp1984 <showp1984@gmail.com>
2012-03-27 01:11:34 +0200

    kgsl: 3D: increase bandwidth to avoid bottlenecking the gpu

--- arch/arm/mach-msm/devices-msm8x60.c
+++ arch/arm/mach-msm/devices-msm8x60.c
@@ -764,7 +764,7 @@ static struct msm_bus_vectors grp3d_nominal_high_vectors[] = {
 		.src = MSM_BUS_MASTER_GRAPHICS_3D,
 		.dst = MSM_BUS_SLAVE_EBI_CH0,
 		.ab = 0,
-		.ib = KGSL_CONVERT_TO_MBPS(2008),
+		.ib = KGSL_CONVERT_TO_MBPS(2484),
 	},
 };
 
@@ -773,7 +773,7 @@ static struct msm_bus_vectors grp3d_max_vectors[] = {
 		.src = MSM_BUS_MASTER_GRAPHICS_3D,
 		.dst = MSM_BUS_SLAVE_EBI_CH0,
 		.ab = 0,
-		.ib = KGSL_CONVERT_TO_MBPS(2484),
+		.ib = KGSL_CONVERT_TO_MBPS(2976),
 	},
 };
 
