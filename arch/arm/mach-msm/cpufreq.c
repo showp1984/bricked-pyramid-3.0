13710fe87efb42d069c6b73fd347dfc2b7d90dc4
showp1984 <showp1984@gmail.com>
2012-04-02 10:23:07 +0200

    cmdline_khz: update governor parsing and cpufreq interface

--- arch/arm/mach-msm/cpufreq.c
+++ arch/arm/mach-msm/cpufreq.c
@@ -126,60 +126,25 @@ __setup("minkhz=", cpufreq_read_minkhz_cmdline);
 
 static int __init cpufreq_read_gov_cmdline(char *gov)
 {
-/* TODO: Find a more elegant way to search for the governor */
-	int found;
-
+	int ret;
 	if (gov) {
-#ifdef CONFIG_CPU_FREQ_GOV_PERFORMANCE
-		if (strcmp(gov, "performance") == 0) {
-			found=1;
-		}
-#endif
-#ifdef CONFIG_CPU_FREQ_GOV_POWERSAVE
-		if (strcmp(gov, "powersafe") == 0) {
-			found=1;
-		}
-#endif
-#ifdef CONFIG_CPU_FREQ_GOV_USERSPACE
-		if (strcmp(gov, "userspace") == 0) {
-			found=1;
-		}
-#endif
-#ifdef CONFIG_CPU_FREQ_GOV_ONDEMAND
-		if (strcmp(gov, "ondemand") == 0) {
-			found=1;
-		}
-#endif
-#ifdef CONFIG_CPU_FREQ_GOV_INTERACTIVE
-		if (strcmp(gov, "interactive") == 0) {
-			found=1;
-		}
-#endif
-#ifdef CONFIG_CPU_FREQ_GOV_CONSERVATIVE
-		if (strcmp(gov, "conservative") == 0) {
-			found=1;
-		}
-#endif
-#ifdef CONFIG_CPU_FREQ_GOV_LAZY
-		if (strcmp(gov, "lazy") == 0) {
-			found=1;
-		}
-#endif
-#ifdef CONFIG_CPU_FREQ_GOV_LAGFREE
-		if (strcmp(gov, "lagfree") == 0) {
-			found=1;
-		}
-#endif
-		if (found == 1) {
-			cmdline_gov = *gov;
+		ret = set_cmdline_governor(gov);
+		if (ret == 0) {
 			printk(KERN_INFO "[cmdline_gov]: Governor set to '%s'", gov);
-		} else {
-			printk(KERN_INFO "[cmdline_gov]: ERROR! Could not find governor '%s'", gov);
+		} else if (ret == 1) {
+			printk(KERN_INFO "[cmdline_gov]: ERROR! Found malformed input '%s'", gov);
+		} else if (ret == 2) {
+			printk(KERN_INFO "[cmdline_gov]: ERROR! Could not get policy for [CPU0]");
+		} else if (ret == 3) {
+			printk(KERN_INFO "[cmdline_gov]: ERROR! Could not get policy for [CPU1]");
+		} else if (ret == 4) {
+			printk(KERN_INFO "[cmdline_gov]: ERROR! Could not get policy!");
+		} else if (ret == 5) {
+			printk(KERN_INFO "[cmdline_gov]: ERROR! Could not set governor '%s'", gov);
 		}
 	} else {
 		printk(KERN_INFO "[cmdline_gov]: No input found.");
 	}
-
 	return 1;
 }
 __setup("gov=", cpufreq_read_gov_cmdline);
--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -1465,6 +1465,48 @@ static struct syscore_ops cpufreq_syscore_ops = {
 	.resume		= cpufreq_bp_resume,
 };
 
+/**
+ * cmdline_khz governor interface
+ *
+ * set_cmdline_governor - store policy for the specified CPU
+ */
+int set_cmdline_governor(const char *buf)
+{
+	int ret, sibling;
+	char	str_governor[16];
+	struct cpufreq_policy *policy;
+	struct cpufreq_policy new_policy;
+
+	ret = sscanf(buf, "%15s", str_governor);
+	if (ret != 1)
+		return 1;
+
+	for_each_online_cpu(sibling) {
+		policy = cpufreq_cpu_get(sibling);
+		if (!policy) {
+			if (sibling == 0) {
+				return 2;
+			} else if (sibling == 1) {
+				return 3;
+			} else {
+				return 4;
+			}
+		}
+		if (cpufreq_parse_governor(str_governor, &new_policy.policy,
+							&new_policy.governor))
+			return 5;
+
+		/* Do not use cpufreq_set_policy here or the user_policy.max
+		will be wrongly overridden */
+		ret = __cpufreq_set_policy(policy, &new_policy);
+
+		policy->user_policy.policy = policy->policy;
+		policy->user_policy.governor = policy->governor;
+	}
+
+	return 0;
+}
+
 
 /*********************************************************************
  *                     NOTIFIER LISTS INTERFACE                      *
