747ea8c83d93f4f3b3254448db5a77bf7ed99b3f
showp1984 <showp1984@gmail.com>
2012-04-04 12:35:11 +0200

    cmdline_maxkhz: Remove double extern declaration for min/max

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -50,9 +50,6 @@ static DEFINE_PER_CPU(struct cpufreq_cpu_save_data, cpufreq_policy_save);
 #endif
 static DEFINE_SPINLOCK(cpufreq_driver_lock);
 
-//cmdline_khz
-extern uint32_t cmdline_maxkhz, cmdline_minkhz;
-
 /*
  * cpu_policy_rwsem is a per CPU reader-writer semaphore designed to cure
  * all cpufreq/hotplug/workqueue/etc related lock issues.
