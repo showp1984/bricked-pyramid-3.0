8f526f02ff5bfb547af8f22ab597a7cb8e50d436
faux123 <reioux@gmail.com>
2012-04-06 00:44:03 -0700

    Bluetooth: Send power off notification earlier in the device close process
    Notifying Bluez userspace of a shutdown earlier prevents new commands from
    being sent. The extra commands were interfering with reinitialization of
    the device
    
    Change-Id: I862a658585ffa69348fbb4bdfba96f3916d0e5ef
    Signed-off-by: Bhasker Neti <bneti@codeaurora.org>
    modified by faux123

--- net/bluetooth/hci_core.c
+++ net/bluetooth/hci_core.c
@@ -652,6 +652,12 @@ static int hci_dev_do_close(struct hci_dev *hdev, u8 is_process)
 
 	hci_notify(hdev, HCI_DEV_DOWN);
 
+	if (hdev->dev_type == HCI_BREDR) {
+		hci_dev_lock_bh(hdev);
+		mgmt_powered(hdev->id, 0);
+		hci_dev_unlock_bh(hdev);
+	}
+
 	if (hdev->flush)
 		hdev->flush(hdev);
 
@@ -683,12 +689,6 @@ static int hci_dev_do_close(struct hci_dev *hdev, u8 is_process)
 	/* After this point our queues are empty
 	 * and no tasks are scheduled. */
 	hdev->close(hdev);
-
-	if (hdev->dev_type == HCI_BREDR) {
-		hci_dev_lock_bh(hdev);
-		mgmt_powered(hdev->id, 0);
-		hci_dev_unlock_bh(hdev);
-	}
 
 	/* Clear only non-persistent flags */
 	if (test_bit(HCI_MGMT, &hdev->flags))
