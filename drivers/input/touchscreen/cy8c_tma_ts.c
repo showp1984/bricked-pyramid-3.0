a2f28d74fc0bc1b610c4d845d0e347b4c2ab40b2
showp1984 <showp1984@gmail.com>
2012-04-10 14:52:47 +0200

    sweep2wake: allow the touchscreen irq to wake the phone

--- drivers/input/touchscreen/cy8c_tma_ts.c
+++ drivers/input/touchscreen/cy8c_tma_ts.c
@@ -1276,6 +1276,7 @@ static int cy8c_ts_suspend(struct i2c_client *client, pm_message_t mesg)
 {
 #ifdef CONFIG_TOUCHSCREEN_CYPRESS_SWEEP2WAKE
 	scr_suspended = true;
+	enable_irq_wake(client->irq);
 #else
 	struct cy8c_ts_data *ts = i2c_get_clientdata(client);
 	uint8_t buf[2] = {0};
@@ -1315,6 +1316,7 @@ static int cy8c_ts_resume(struct i2c_client *client)
 {
 #ifdef CONFIG_TOUCHSCREEN_CYPRESS_SWEEP2WAKE
 	scr_suspended = false;
+	disable_irq_wake(client->irq);
 #else
 	struct cy8c_ts_data *ts = i2c_get_clientdata(client);
 	uint8_t buf[2] = {0};
