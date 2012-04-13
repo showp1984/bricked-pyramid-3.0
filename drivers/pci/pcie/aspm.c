2dad8e932960578be8786ed57637823e4acb6aae
showp1984 <showp1984@gmail.com>
2012-04-13 12:20:19 +0200

    Patching Linux 3.0.26 to upstream 3.0.27
    Conflicts:
    
    	Makefile
    	drivers/bluetooth/ath3k.c
    	drivers/bluetooth/btusb.c
    	drivers/gpu/drm/radeon/radeon_connectors.c
    	drivers/usb/serial/cp210x.c
    	fs/nfs/nfs4proc.c

--- drivers/pci/pcie/aspm.c
+++ drivers/pci/pcie/aspm.c
@@ -500,9 +500,6 @@ static int pcie_aspm_sanity_check(struct pci_dev *pdev)
 	int pos;
 	u32 reg32;
 
-	if (aspm_disabled)
-		return 0;
-
 	/*
 	 * Some functions in a slot might not all be PCIe functions,
 	 * very strange. Disable ASPM for the whole slot
