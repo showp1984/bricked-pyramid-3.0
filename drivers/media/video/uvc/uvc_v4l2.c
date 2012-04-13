00898684dec772ec876983b669f9300eba8986a8
showp1984 <showp1984@gmail.com>
2012-04-13 12:18:02 +0200

    FIXUP: Patching Linux 3.0.25 to upstream 3.0.26
    This code was added twice, since we don't use it it wasn't discovered
    immediatly. But it does complicate future upstream updates, do the
    right thing and correct the mistake.

--- drivers/media/video/uvc/uvc_v4l2.c
+++ drivers/media/video/uvc/uvc_v4l2.c
@@ -81,16 +81,6 @@ static int uvc_ioctl_ctrl_map(struct uvc_video_chain *chain,
 		    xmap->menu_count > UVC_MAX_CONTROL_MENU_ENTRIES) {
 			ret = -EINVAL;
 			goto done;
-		}
-
-		/* Prevent excessive memory consumption, as well as integer
-		 * overflows.
-		 */
-		if (xmap->menu_count == 0 ||
-		    xmap->menu_count > UVC_MAX_CONTROL_MENU_ENTRIES) {
-			ret = -EINVAL;
-			goto done;
-		}
 
 		size = xmap->menu_count * sizeof(*map->menu_info);
 		map->menu_info = kmalloc(size, GFP_KERNEL);
