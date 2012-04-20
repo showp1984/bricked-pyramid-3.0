143092833432f4e3547ca5471fc6e5e57d74b6c1
showp1984 <showp1984@gmail.com>
2012-04-20 03:59:17 +0200

    badass: fixup setting initial 2phase freq

--- arch/arm/mach-msm/board-pyramid.c
+++ arch/arm/mach-msm/board-pyramid.c
@@ -6209,7 +6209,9 @@ static void __init msm8x60_init(struct msm_board_data *board_data)
 
 #ifdef CONFIG_CPU_FREQ_GOV_ONDEMAND_2_PHASE
 	set_two_phase_freq(1134000);
-#elif defined(CONFIG_CPU_FREQ_GOV_BADASS_2_PHASE)
+#endif
+
+#ifdef CONFIG_CPU_FREQ_GOV_BADASS_2_PHASE
 	set_two_phase_freq_badass(CONFIG_CPU_FREQ_GOV_BADASS_2_PHASE_FREQ);
 #endif
 #ifdef CONFIG_CPU_FREQ_GOV_BADASS_3_PHASE
