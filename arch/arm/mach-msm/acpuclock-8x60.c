9bdc428989e2092db58a252d5045ed2464d2d4e3
showp1984 <showp1984@gmail.com>
2012-06-23 16:39:21 +0200

    mach-msm: lower min POSSIBLE cpu voltage (provides a wider scaling range for the end user)
    Signed-off-by: showp1984 <showp1984@gmail.com>

--- arch/arm/mach-msm/acpuclock-8x60.c
+++ arch/arm/mach-msm/acpuclock-8x60.c
@@ -46,7 +46,7 @@
 #define L_VAL_SCPLL_CAL_MAX	0x24 /* = 1944 MHz with 27MHz source */
 
 #define MAX_VDD_SC		1450000 /* uV */
-#define MIN_VDD_SC		 750000 /* uV */
+#define MIN_VDD_SC		 700000 /* uV */
 #define MAX_VDD_MEM		1450000 /* uV */
 #define MAX_VDD_DIG		1300000 /* uV */
 #define MAX_AXI			 310500 /* KHz */
--- arch/arm/mach-msm/board-pyramid.c
+++ arch/arm/mach-msm/board-pyramid.c
@@ -497,7 +497,7 @@ static struct regulator_init_data saw_s0_init_data = {
 		.constraints = {
 			.name = "8901_s0",
 			.valid_ops_mask = REGULATOR_CHANGE_VOLTAGE,
-			.min_uV = 750000,
+			.min_uV = 700000,
 			.max_uV = 1450000,
 		},
 		.consumer_supplies = vreg_consumers_8901_S0,
@@ -508,7 +508,7 @@ static struct regulator_init_data saw_s1_init_data = {
 		.constraints = {
 			.name = "8901_s1",
 			.valid_ops_mask = REGULATOR_CHANGE_VOLTAGE,
-			.min_uV = 750000,
+			.min_uV = 700000,
 			.max_uV = 1450000,
 		},
 		.consumer_supplies = vreg_consumers_8901_S1,
